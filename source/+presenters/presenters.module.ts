a7fbf375d2e88e8c693158452984ace26357639e
SamGraber <authorsamgraber@gmail.com>
2016-09-16 15:48:05 -0400

    Added the presenter components and resolvers to the presenters module

--- source/+presenters/presenters.module.ts
+++ source/+presenters/presenters.module.ts
@@ -3,6 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { ComponentsModule } from '../components/components.module';
 import { presenterRouting } from './routing/presenters.routing';
+import { PresenterListComponent } from './presenter-list/presenter-list.component';
+import { PresenterComponent } from './presenter/presenter.component';
+import { PresenterListResolver } from './routing/presenter-list.resolver';
+import { PresenterResolver } from './routing/presenter.resolver';
 import { PresenterService } from './shared/presenter.service';
 
 @NgModule({
@@ -11,8 +15,14 @@ import { PresenterService } from './shared/presenter.service';
 		ComponentsModule,
 		presenterRouting,
 	],
+	declarations: [
+		PresenterListComponent,
+		PresenterComponent,
+	],
 	providers: [
 		PresenterService,
+		PresenterListResolver,
+		PresenterResolver,
 	],
 })
 export default class PresenterModule { }
